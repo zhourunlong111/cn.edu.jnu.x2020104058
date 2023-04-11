1babf23c9f77f60d2e43dab091bc4c203be45a23
zhourunlong111 <1024547461@qq.com>
2023-04-11 22:50:23 +0800

    第五次测试通过

--- app/src/main/java/com/example/cnedujnux2020104058/MainActivity.java
+++ app/src/main/java/com/example/cnedujnux2020104058/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.cnedujnux2020104058;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonCalculate=this.findViewById(R.id.button_calculate);
+        final EditText editTextScores=this.findViewById(R.id.edittext_frame_scores);
+        final TextView textViewScore=this.findViewById(R.id.textview_score);
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String scores=editTextScores.getText().toString();
+                String []arrayScore=scores.split(",");
+                BowlingGame game=new BowlingGame();
+                for(int i=0;i<arrayScore.length;i++){
+                    game.roll(Integer.parseInt(arrayScore[i]));
+                }
+                textViewScore.setText("Total score is:"+game.score());
+            }
+        });
     }
 }
